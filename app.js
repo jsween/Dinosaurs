d4cfcbb1a06f690054e344289768ae9b57c341ef
Jon Sweeney <jonsweeney26@gmail.com>
2021-05-26 18:04:51 -0700

    add compare weight function

--- app.js
+++ app.js
@@ -35,19 +35,9 @@ function Human(species, wt, ht, diet, name) {
     this.name = name;
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.getElementById('btn').addEventListener('click', function() {
-//         const formData = new FormData(document.querySelector('#dino-compare'));
-//         const name = formData.get('name');
-//         const inches = Number(formData.get('feet')) * 12 + Number(formData.get('inches'));
-//         const weight = formData.get('weight');
-//         const diet = formData.get('diet');
-//         console.log(`${name} ${inches}" ${weight}lbs ${diet}`)
-//
-//         return new Human("homo sapien", name, weight, inches, diet);
-//     })
-// });
-
+/*
+    @desc Get Human data with IIFE
+*/
 let human = null;
 const compareBtn = document.getElementById('btn');
 
@@ -60,10 +50,26 @@ compareBtn.addEventListener('click', (function() {
         const diet = formData.get('diet');
 
         human = new Human("homo sapien", name, weight, inches, diet);
-        compareWeight(human);
+        compareWeight(dino);
     };
 })());
 
-function compareWeight(human) {
-    console.log(human);
+/*
+    @desc Compare dinosaur's weight to user's
+    @param Dino $dino - An instance of a Dinosaur
+*/
+
+function compareWeight(dino) {
+    const dinoWt = Number(dino.weight);
+    const dinoHt = Number(dino.height);
+
+    if (dinoWt > human.weight) {
+        console.log("Dino is heavier");
+        return `${dino.species} is ${Math.round(dinoWt / human.weight)} times heavier than you!`;
+    } else if (dinoWt < human.weight) {
+        console.log("User is heavier");
+        return `You are ${Math.round(dinoWt / human.weight)} times heavier than a ${dino.species}!`;
+    } else {
+        console.log(`You are as heavy as a ${dino.species}!`);
+    }
 }
